9e2a4ffbedab55ea421c1d8ce3746542b9132eef
Aleksander Storum <alesto95654@stud.noroff.no>
2024-05-26 14:12:25 +0200

    Added a home link to NavLinks on small screens. Also added some styling to the hamburger menu.

--- src/components/nav-links/NavLinks.jsx
+++ src/components/nav-links/NavLinks.jsx
@@ -3,7 +3,7 @@ import * as S from "./NavLinks.styles";
 
 /**
  * Component for rendering navigation links. This component displays links to the Home and Contact pages.
- * It receives a callback function `onClose` that is called when a link is clicked, closing mobile navigation menu.
+ * It receives a callback function `onClose` that is called when a link is clicked, closing the mobile navigation menu.
  *
  * @param {Object} props - Component props
  * @param {Function} props.onClose - Function to call when a link is clicked
@@ -13,8 +13,19 @@ import * as S from "./NavLinks.styles";
  * <NavLinks onClose={() => console.log('Navigating away...')} />
  */
 const NavLinks = ({ onClose, isOpen, isHeaderHovered }) => {
+  // Handling mouse leave event
+  const handleMouseLeave = () => {
+    if (window.innerWidth <= 768) {
+      // Check if the screen width is 768px or less
+      onClose(); // Close the navigation menu
+    }
+  };
+
   return (
-    <S.NavContainer isOpen={isOpen}>
+    <S.NavContainer isOpen={isOpen} onMouseLeave={handleMouseLeave}>
+      <S.StyledLinkHome to="/" onClick={onClose} isHovered={isHeaderHovered}>
+        Home
+      </S.StyledLinkHome>
       <S.StyledLink to="/venues" onClick={onClose} isHovered={isHeaderHovered}>
         Venues
       </S.StyledLink>
